3bd454e14104cbd366c3343321cd8692a59ffc53
Dylan Tettarasar <dtettarasar@gmail.com>
2021-01-23 16:12:49 +0100

    added go to top button

---
+++ js/script.js
@@ -0,0 +1,20 @@
+// script for go to top button
+
+var btnTop = document.getElementById('btn-top');
+
+function goToTop(){
+  window.scrollTo(0, 0);
+}
+
+function displayBtn(){
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    btnTop.style.display = 'block';
+  }else{
+    btnTop.style.display = 'none';
+  }
+}
+
+window.onscroll = function(){displayBtn()};
+btnTop.addEventListener("click",goToTop);
+
+// end of script for go to top button
